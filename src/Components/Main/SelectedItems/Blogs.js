4fa97c2fc56434100feaf92b5c78cd12bcc68988
Prachi Bala <prachibala330@gmail.com>
2022-09-29 23:45:45 +0600

    ver-0.9

--- src/Components/Main/SelectedItems/Blogs.js
+++ src/Components/Main/SelectedItems/Blogs.js
@@ -46,7 +46,7 @@ const Blogs = () => {
                             aria-expanded="false"
                             aria-controls="collapseTwo"
                         >
-                            Accordion Item #2
+                            difference between props and state?
                         </button>
                     </h2>
                     <div
@@ -64,41 +64,6 @@ const Blogs = () => {
                             local to the component only and cannot be passed to
                             other components. The this.setState property is used
                             to update the state values in the component.
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingThree">
-                        <button
-                            className="accordion-button collapsed"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#collapseThree"
-                            aria-expanded="false"
-                            aria-controls="collapseThree"
-                        >
-                            Accordion Item #3
-                        </button>
-                    </h2>
-                    <div
-                        id="collapseThree"
-                        className="accordion-collapse collapse"
-                        aria-labelledby="headingThree"
-                        data-bs-parent="#accordionExample"
-                    >
-                        <div className="accordion-body">
-                            <strong>
-                                This is the third item's accordion body.
-                            </strong>{" "}
-                            It is hidden by default, until the collapse plugin
-                            adds the appropriate classes that we use to style
-                            each element. These classes control the overall
-                            appearance, as well as the showing and hiding via
-                            CSS transitions. You can modify any of this with
-                            custom CSS or overriding our default variables. It's
-                            also worth noting that just about any HTML can go
-                            within the <code>.accordion-body</code>, though the
-                            transition does limit overflow.
                         </div>
                     </div>
                 </div>
