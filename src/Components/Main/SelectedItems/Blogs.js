5ccd798e465f72bc55b854887cef9cadf620f9fe
Prachi Bala <prachibala330@gmail.com>
2022-09-29 23:42:06 +0600

    ver-0.8

--- src/Components/Main/SelectedItems/Blogs.js
+++ src/Components/Main/SelectedItems/Blogs.js
@@ -14,7 +14,7 @@ const Blogs = () => {
                             aria-expanded="true"
                             aria-controls="collapseOne"
                         >
-                            Accordion Item #1
+                            How does React work?
                         </button>
                     </h2>
                     <div
@@ -26,16 +26,13 @@ const Blogs = () => {
                         <div className="accordion-body">
                             <strong>
                                 This is the first item's accordion body.
-                            </strong>{" "}
-                            It is shown by default, until the collapse plugin
-                            adds the appropriate classes that we use to style
-                            each element. These classes control the overall
-                            appearance, as well as the showing and hiding via
-                            CSS transitions. You can modify any of this with
-                            custom CSS or overriding our default variables. It's
-                            also worth noting that just about any HTML can go
-                            within the <code>.accordion-body</code>, though the
-                            transition does limit overflow.
+                            </strong>
+                            ReactJS divides the UI into isolated reusable pieces
+                            of code known as components. React components work
+                            similarly to JavaScript functions as they accept
+                            arbitrary inputs called properties or props. It's
+                            possible to have as many components as necessary
+                            without cluttering your code.
                         </div>
                     </div>
                 </div>
